20cc8e0f47bf310d5bd386034983c5ea653f6c19
SpicyRicecaker <awesomenessisgolden@gmail.com>
2021-01-24 22:04:25 -0800

    disc-17-william-res

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,16 @@
-import week from "./days.ts";
-import ClassDB from "./classDB.ts";
+import week from './days.ts';
+import ClassDB from './classDB.ts';
 
 // parse and compare minute hour string
 // given hours and minutes of the form
 const isInsideTime = (start: string, end: string, date: Date): boolean => {
   // Get start hour, start minutes, end hour, end minutes
-  const s = [...start.split(":"), ...end.split(":")];
+  const s = [...start.split(':'), ...end.split(':')];
   const e = s.map((value) => parseInt(value, 10));
   // Get the amount of time in class by subtracting start by endtime
-  const during = e[2] * 60 + e[3] -
-    (e[0] * 60 + e[1]);
+  const during = e[2] * 60 + e[3] - (e[0] * 60 + e[1]);
   // Gets the amount of time between now and the beginning of class
-  const until = date.getHours() * 60 +
-    date.getMinutes() -
-    (e[0] * 60 + e[1]);
+  const until = date.getHours() * 60 + date.getMinutes() - (e[0] * 60 + e[1]);
   // If the time between now and the beginning of class is greater than -6 (5 min early)
   // but less than the full length of the actual class, then this class is within time
   return until >= -Deno.args[1] && until <= during;
@@ -29,6 +26,11 @@ const main = async () => {
   // First get today's date
   let today = new Date();
 
+  const day = `${today.toLocaleString('default', {
+    month: 'long',
+  })}`;
+  console.log(day);
+  return;
   // Compile our classes database
   const classDB = new ClassDB();
   await classDB.init();
